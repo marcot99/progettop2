bb3eb72ca72b4fc6db8edce97ad6d3583ccea917
marcot99 <65403402+marcot99@users.noreply.github.com>
2020-05-21 12:58:41 +0200

    Delete onnivoro.cpp

--- onnivoro.cpp
+++
@@ -1,6 +0,0 @@
-#include "onnivoro.h"
-
-
-
-Onnivoro::Onnivoro(int Pe, int A, int E, int Pb, std::string Cs, bool Ie, std::string S, bool V, std::string Pr, list<Tipo> t, bool ve, bool ne):
-    Animale(Pe,A,E,Pb,Cs,Ie,S,V,Pr),Erbivoro(Pe,A,E,Pb,Cs,Ie,S,V,Pr,t),Carnivoro(Pe,A,E,Pb,Cs,Ie,S,V,Pr,ve,ne){}
