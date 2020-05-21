d498cd24d93ccf6176c56365d57661ea08031f03
marcot99 <65403402+marcot99@users.noreply.github.com>
2020-05-21 12:57:55 +0200

    Delete carnivoro.cpp

--- carnivoro.cpp
+++
@@ -1,8 +0,0 @@
-#include "carnivoro.h"
-
-Carnivoro::Carnivoro(int Pe, int A, int E, int Pb, std::string Cs, bool Ie, std::string S, bool V, std::string Pr, bool ve, bool ne):
-    Animale(Pe,A,E,Pb,Cs,Ie,S,V,Pr),velenoso(ve),necrofago(ne){}
-
-bool Carnivoro::getvelenoso() const {return velenoso;}
-
-bool Carnivoro::getnecrofago() const {return necrofago;}
